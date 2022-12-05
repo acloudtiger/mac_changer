fb96cba4c323bdc2552bccbb74f95659f0b9e655
Michael Chang <acloudtiger@gmail.com>
2022-12-05 02:56:36 -0500

    Add Variables for Interface and New MAC address

--- mac_changer.py
+++ mac_changer.py
@@ -10,6 +10,6 @@ new_mac="00:11:22:33:44:77"
 print("[+] Change MAC address for " + interface + " to " + new_mac)
 
 # ifconfig command
-subprocess.call("ifconfig "+ interface +" down", shell=True)
-subprocess.call("ifconfig "+ interface +"  hw ether " + new_mac + "00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig "+ interface +"  up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + "  hw ether " + new_mac + "00:11:22:33:44:66", shell=True)
+subprocess.call("ifconfig " + interface + "  up", shell=True)
