c5be735eac39dcb653ecf8249d84729fb691184d
Michael Chang <acloudtiger@gmail.com>
2022-12-05 22:17:39 -0500

    25 using Conditional statements for mac changer

--- mac_changer.py
+++ mac_changer.py
@@ -10,7 +10,12 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
     # parse_args : method
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return options
 
 
 # Rewrite by using function to change mac
@@ -22,5 +27,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
