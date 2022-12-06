4fe6ae937e082c07f0731f248cbd949079d2283d
Michael Chang <acloudtiger@gmail.com>
2022-12-06 18:02:40 +0800

    31 Implementing The Valid Algorithm

--- mac_changer.py
+++ mac_changer.py
@@ -26,12 +26,24 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_serach_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_serach_result:
+        return mac_address_serach_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+        return "No MAC address for the interface"
+
+
 options = get_arguments()
-# change_mac(options.interface, options.new_mac)
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
-mac_address_serach_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-if mac_address_serach_result:
-    print(mac_address_serach_result.group(0))
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + current_mac)
+change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully change to " + current_mac)
 else:
-    print("[-] Could not read MAC address.")
+    print("[-] MAC address did not get changed")
