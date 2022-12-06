6561f802b8aa7b61f7a3c5bc883ecb588d6cfe2a
Michael Chang <acloudtiger@gmail.com>
2022-12-06 01:18:17 -0500

    29 re.search find mac address in ifconfig

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -27,5 +28,10 @@ def change_mac(interface, new_mac):
 
 options = get_arguments()
 # change_mac(options.interface, options.new_mac)
-ifconfig_result = subprocess.check_call(["ifconfig", options.interface])
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
 print(ifconfig_result)
+mac_address_serach_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+if mac_address_serach_result:
+    print(mac_address_serach_result.group(0))
+else:
+    print("[-] Could not read MAC address.")
