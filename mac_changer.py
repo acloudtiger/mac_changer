6e7d47e050e6f1d6a460ac057728f7b733ffc4a6
Michael Chang <acloudtiger@gmail.com>
2022-12-05 21:40:47 -0500

    rewrite by function

--- mac_changer.py
+++ mac_changer.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
-
 import subprocess
+import optparse
 
-# Input for Variables (2.7 using raw_input command , 3.X using input command)
-# Variables for interface as Eth0
-# Variables for the new MAC address
 
-# interface = input("Interface > ")
-# new_mac= input("New MAC > ")
+# Rewrite by using function to change mac
+def change_mac(interface, new_mac):
+    print("[+] Change MAC address for " + interface + " to " + new_mac)
+    # using another method for security issue, eg eth0;ls; for input
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-interface = raw_input("Interface > ")
-new_mac= raw_input("New MAC > ")
 
-print("[+] Change MAC address for " + interface + " to " + new_mac)
+# OptionParser(): Class
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
 
-# ifconfig command
-# subprocess.call("ifconfig " + interface + " down", shell=True)
-# subprocess.call("ifconfig " + interface + "  hw ether " + new_mac + "00:11:22:33:44:66", shell=True)
-# subprocess.call("ifconfig " + interface + "  up", shell=True)
+# parse_args : method
+(options, arguments) = parser.parse_args()
 
-# using another method for security issue, eg eth0;ls; for input
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw",  "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.new_mac)
