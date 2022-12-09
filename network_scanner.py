f97a985300072b06b33b4f615949b3b2c86e4f3a
Michael Chang <acloudtiger@gmail.com>
2022-12-09 12:17:57 +0800

    38 Combining Frames to broadcast packets

--- network_scanner.py
+++ network_scanner.py
@@ -4,8 +4,16 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    # scapy.ls(scapy.ARP()) to check pdst is for ip address
-    print(arp_request.summary())
+    # scapy.ls(scapy.ARP())
+    arp_request.show()
+
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # scapy.ls(broadcast)
+    broadcast.show()
+
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
+
 
 
 
