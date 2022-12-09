98085f792376c94bd9d819cf8e2544a92f6f2472
Michael Chang <acloudtiger@gmail.com>
2022-12-09 09:26:02 +0800

    32 Python3 compatibility

--- mac_changer.py
+++ mac_changer.py
@@ -28,7 +28,7 @@ def change_mac(interface, new_mac):
 
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_serach_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_serach_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
     if mac_address_serach_result:
         return mac_address_serach_result.group(0)
     else:
