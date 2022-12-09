2b9d656a2d86992506b60f79d3a41577abc9434d
Michael Chang <acloudtiger@gmail.com>
2022-12-09 14:36:50 +0800

    39 Sending and Receiving Packets

--- network_scanner.py
+++ network_scanner.py
@@ -4,17 +4,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    # scapy.ls(scapy.ARP())
-    arp_request.show()
-
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    # scapy.ls(broadcast)
-    broadcast.show()
-
     arp_request_broadcast = broadcast/arp_request
-    arp_request_broadcast.show()
-
-
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 
 scan("192.168.154.2/24")
