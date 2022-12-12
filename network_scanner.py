c4f2edf2db4b10d7710b03de9620af8500780335
Michael Chang <acloudtiger@gmail.com>
2022-12-12 15:57:49 +0800

    42 Using Escape Characters to improve program output

--- network_scanner.py
+++ network_scanner.py
@@ -7,12 +7,13 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     # scapy.srp get 2 lists, using scapy.srp()[0] to get the first list, with is answered_list
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose= False)[0]
+
+    print("IP\t\t\tMAC Address\n--------------------------------------------")
     for element in answered_list:
         # print(element[1].show()) to find the psrc, hwsrc
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("--------------------------------------------")
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    print("--------------------------------------------")
 
 
 scan("192.168.154.2/24")
