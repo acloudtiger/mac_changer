46a8fbd0965cd41f46ac940a3a6c62fe274a1b04
Michael Chang <acloudtiger@gmail.com>
2022-12-12 16:27:35 +0800

    45 Iterating over nested data structures

--- network_scanner.py
+++ network_scanner.py
@@ -9,14 +9,20 @@ def scan(ip):
     # scapy.srp get 2 lists, using scapy.srp()[0] to get the first list, with is answered_list
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose= False)[0]
 
-    print("IP\t\t\tMAC Address\n--------------------------------------------")
+
     clients_list = []
     for element in answered_list:
         client_dict = {"ip" : element[1].psrc, "mac" : element[1].hwsrc}
         clients_list.append(client_dict)
         # print(element[1].show()) to find the psrc, hwsrc
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-    print(clients_list)
+    return clients_list
 
 
-scan("192.168.154.2/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n--------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+scan_result = scan("192.168.154.2/24")
+print_result(scan_result)
