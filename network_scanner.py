f5d21dc1733c74323d4d2ef56b46c2e5363a2a62
Michael Chang <acloudtiger@gmail.com>
2022-12-12 15:48:12 +0800

    41 Iterating over lists and analysing packet

--- network_scanner.py
+++ network_scanner.py
@@ -6,8 +6,13 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    # scapy.srp get 2 lists, using scapy.srp()[0] to get the first list, with is answered_list
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    for element in answered_list:
+        # print(element[1].show()) to find the psrc, hwsrc
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("--------------------------------------------")
 
 
 scan("192.168.154.2/24")
