05811a9f5cebee66147b8adb82e2fda83265be40
Michael Chang <acloudtiger@gmail.com>
2023-01-06 16:16:05 +0800

    69. Filtering DNS Responses

--- dns_spoof.py
+++ dns_spoof.py
@@ -7,7 +7,8 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        print(scapy_packet.show())
     packet.accept()
 
 
